de499385f856975eb3a283b4929db0fb20ad5e65
nelasunitha <80846358+nelasunitha@users.noreply.github.com>
2023-10-02 23:21:34 -0700

    homework09 completed

--- Homework/homework09.js
+++ Homework/homework09.js
@@ -13,7 +13,7 @@ fizzBuzz1(10)  -> "Buzz"
 fizzBuzz1(15)  -> "FizzBuzz"*/
 console.log('Output for Task1\n');
 function fizzBuzz1(num) {
-  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if(num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
   else if (num % 3 === 0) return 'Fizz';
   else if (num % 5 === 0) return 'Buzz';
   else return num;
@@ -69,146 +69,220 @@ findSumNumbers("525") -> 525
 findSumNumbers("3 for 10 dollars") -> 13
  */
 console.log('\nOutput for Task3');
-function findSumNumbers(str){
-  let num ='',sum =0
-  for(let i = 0; i< str.length;i++) {
-    if(!isNaN(str[i])) {
-      num += str[i]
-    }else if(num !== '') {
-      sum+= +num;
-      num = ""
+function findSumNumbers(str) {
+  let num = '',
+    sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      num += String(str[i]);
+    } else if (num !== '') {
+      sum += +num;
+      num = '';
     }
   }
-  if(num !== '') sum+= +num;
-   return sum;
+  if (num !== '') sum += +num;
+  return sum;
 }
-console.log(findSumNumbers("abc$"));
-console.log(findSumNumbers("a1b4c  6#"));
-console.log(findSumNumbers("ab110c045d"));
-console.log(findSumNumbers("525"));
-console.log(findSumNumbers("3 for 10 dollars"));
-// console.log(noVowel("AEOxyz"));
-// console.log(noVowel("Javascript"));
-// console.log(noVowel(""))
-// console.log(noVowel("125$"));
-// /* Task 4
-// Requirement
-// Write a function named no13() which takes an array of numbers as argument
-// and return a new array with all 13s replaced with 0s.
-// Examples:
-// no13([1, 2, 3 ,4])  -> [1, 2, 3 ,4]
-// no13([13, 2, 3])  -> [0, 2, 3]
-// no13([13, 13, 13 , 13, 13])  -> [0, 0, 0, 0, 0]
-// no13([])  -> []
-// */
-// console.log('\nOutput for Task4');
-// function no13(nums){
-//   let result = [];
-//   for(let i =0; i < nums.length;i++) {
-//     if(nums[i] === 13) result.push(0);
-//     else result.push(nums[i])
-//   }
-//   return result;
-// }
-// console.log(no13([1, 2, 3 ,4]));
-// console.log(no13([13, 2, 3]) );
-// console.log(no13([13, 13, 13 , 13, 13]));
-// console.log(no13([]));
+console.log(findSumNumbers('abc$'));
+console.log(findSumNumbers('a1b4c  6#'));
+console.log(findSumNumbers('ab110c045d'));
+console.log(findSumNumbers('525'));
+console.log(findSumNumbers('3 for 10 dollars'));
+/* Task 4
+Write a function named findBiggestNumber() which takes a string argument
+and returns the biggest number appears in the string.
+Examples:
+findBiggestNumber("abc$")  -> 0
+findBiggestNumber("a1b4c  6#")  -> 6
+findBiggestNumber("ab110c045d")  -> 110
+findBiggestNumber("525")  -> 525
+findBiggestNumber("3 for 10 dollars")  -> 10
+*/
+console.log('\nOutput for Task4');
+function findBiggestNumber(str) {
+  let num = '',
+    maxNum = -Infinity;
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      num += String(str[i]);
+    } else if (num !== '') {
+      maxNum = Math.max(+num, maxNum);
+      num = '';
+    }
+  }
+  if (num !== '') return +num;
+  if (maxNum == -Infinity) return 0;
+  return maxNum;
+}
+console.log(findBiggestNumber('abc$'));
+console.log(findBiggestNumber('a1b4c  6#'));
+console.log(findBiggestNumber('ab110c045d'));
+console.log(findBiggestNumber('525'));
+console.log(findBiggestNumber('3 for 10 dollars'));
 
-// /* Task 5
-// Requirement:
-// Write a function named middleInt() which takes three number arguments and return the middle number.
-// Examples:
-// middleInt(1, 2, 2)  -> 2
-// middleInt(5, 5, 8)  -> 5
-// middleInt(5, 3, 5)  -> 5
-// middleInt(1, 1, 1)  -> 1
-// middleInt(-1, 25, 10)  -> 10
-// */
-// console.log('\nOutput for Task5');
-// function middleInt(n1, n2, n3){
-//   let result = [n1, n2, n3]
-//   result.sort((a,b) => a - b);
-//   return result[1];
-// }
-// console.log(middleInt(1, 2, 2));
-// console.log(middleInt(5, 5, 8));
-// console.log(middleInt(5, 3, 5));
-// console.log(middleInt(1, 1, 1));
-// console.log(middleInt(-1, 25, 10));
+/* Task 5
+Write a function named countOccurrencesOfCharacters() which takes a
+string argument and returns the count of repeated characters in the String.
+NOTE: If given String is empty, then return empty String.
+NOTE: It is case sensitive.
+Examples:
+countOccurrencesOfCharacters("")  -> ""
+countOccurrencesOfCharacters("abc")  -> "1a1b1c"
+countOccurrencesOfCharacters("abbcca")  -> "1a2b2c1a"
+countOccurrencesOfCharacters("aaAAa")  -> "2a2A1a”
+countOccurrencesOfCharacters("www" ) -> "3w"
+*/
+console.log('\nOutput for Task5 ');
+function countOccurrencesOfCharacters(str) {
+  let result = '';
+  if (str.length === 0) return '';
+  let count = 1;
+  let char = str[0];
+  for (let i = 1; i < str.length; i++) {
+    if (char === str[i]) {
+      count++;
+    } else {
+      result += count + char;
+      char = str[i];
+      count = 1;
+    }
+  }
+  result += count + char;
+  return result;
+}
+console.log(countOccurrencesOfCharacters(''));
+console.log(countOccurrencesOfCharacters('abc'));
+console.log(countOccurrencesOfCharacters('abbcca'));
+console.log(countOccurrencesOfCharacters('aaAAa'));
+console.log(countOccurrencesOfCharacters('www'));
 
-// /* Task 6
-// Requirement:
-// Write a function named sumOfDigits() which takes a string argument and returns sum of all digits from the original string.
-// Examples:
-// sumOfDigits("Javascript")  -> 0
-// sumOfDigits("John’s age is 29")  -> 11
-// sumOfDigits("$125.0")  -> 8
-// sumOfDigits("")  -> 0
-// */
-// console.log('\nOutput for Task6');
-// function sumOfDigits(str){
-//   let sum =0;
-//   for(let i =0; i < str.length; i++){
-//     if(!isNaN(str[i])) sum+= +str[i];
-//     //if(!isNaN(str[i])) sum+= Number(str[i]);
-//   }
-//   return sum;
-// }
-// console.log(sumOfDigits("Javascript"));
-// console.log(sumOfDigits("John’s age is 29"));
-// console.log(sumOfDigits("$125.0"));
-// console.log(sumOfDigits(""));
+/* Task 6
+Requirement:
+Write a function named fibonacciSeries1() which takes a number n argument
+and returns the n series of Fibonacci numbers as an array.
+REMEMBER: Fibonacci series = 0, 1, 1, 2, 3, 5, 8, 13, 21
+Examples:
+fibonacciSeries1(3)  -> [0, 1, 1]
+fibonacciSeries1(5)  -> [0, 1, 1, 2, 3]
+fibonacciSeries1(7)  -> [0, 1, 1, 2, 3, 5, 8]
+fibonacciSeries1(8)  -> [0, 1, 1, 2, 3, 5, 8, 13]
+fibonacciSeries1(1)  -> [0]
+fibonacciSeries1(2)  -> [0, 1]
+*/
+console.log('\nOutput for Task6');
+function fibonacciSeries1(num) {
+  if (num < 1) return 0;
 
-// /* Task 7
-//  Requirement
-// Write a function named arrFactorial() which takes an array of numbers as
-// argument and return the array with every number replaced with their factorials.
-// Examples:
-// arrFactorial([1, 2, 3 ,4])  ->  [1, 2, 6, 24]
-// arrFactorial([0, 5])  -> [1,120]
-// arrFactorial([5 , 0, 6])  -> [120, 1, 720]
-// arrFactorial([])  -> []  */
-// console.log('\nOutput for Task7');
-// function arrFactorial(nums) {
-//   let result = [];
-//   if(nums.length <=1) return nums;
-//   for(let i =0; i < nums.length; i++) {
-//     result[i] = factorial(nums[i])
-//   }
-//   return result;
-// }
-// function factorial(num) {
-//   let fact = 1
-//   while(num > 0) {
-//     fact*= num;
-//     num = num -1;
-//   }
-//   return fact;
-// }
-// console.log(arrFactorial([1, 2, 3 ,4]));
-// console.log(arrFactorial([0, 5]));
-// console.log(arrFactorial([5 , 0, 6]));
-// console.log(arrFactorial([]));
-// /* Task 8
-// Requirement
-// Write a function named categorizeCharacters() which takes a string word as argument and return an array as letters at index of 0, digits at index of 1 and specials at index of 2.
-// Examples:
-// categorizeCharacters("1234")  -> [ '' , '1234', '' ]
-// categorizeCharacters("abc123$#%")  -> [ 'abc', '123', '$#%' ]
-// categorizeCharacters("12ab$%3c%")  -> [ 'abc', '123', '$%%' ]
-//  */
-// console.log('\nOutput for Task8');
-// function categorizeCharacters(word){
-//   let result = ["","",""];
-//    for(let i = 0; i < word.length; i++) {
-//      if((word[i] >= 'A' && word[i] <= 'Z') || (word[i] >= 'a' && word[i] <= 'z'))
-//       result[0] += word[i];
-//     else if(word[i] >= 0 && word[i] <= 9) result[1]+= word[i];
-//     else result[2]+= word[i]
-//    }
-//    return result;
-//  }
-//  console.log(categorizeCharacters("1234"));
-//  console.log(categorizeCharacters("abc123$#%"));
-//  console.log(categorizeCharacters("12ab$%3c%"));
+  let result = Array(num).fill(0);
+  if (num >= 2) {
+    result[0] = 0;
+    result[1] = 1;
+    for (let i = 2; i < num; i++) {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+  }
+  return result;
+}
+console.log(fibonacciSeries1(3));
+console.log(fibonacciSeries1(5));
+console.log(fibonacciSeries1(7));
+console.log(fibonacciSeries1(8));
+console.log(fibonacciSeries1(1));
+console.log(fibonacciSeries1(2));
+/* Task 7
+ Write a function named fibonacciSeries2() which takes a number n argument and returns the nth series of Fibonacci number as a number.
+REMEMBER: Fibonacci series = 0, 1, 1, 2, 3, 5, 8, 13, 21
+
+Examples:
+fibonacciSeries2(2)  -> 1
+fibonacciSeries2(4)  -> 2
+fibonacciSeries2(8)  -> 13
+fibonacciSeries2(9)  -> 21
+fibonacciSeries2(1)  -> 0 */
+console.log('\nOutput for Task7');
+function fibonacciSeries2(num) {
+  // if(num <= 1) return num;
+  // return fibonacciSeries2(num-1) + fibonacciSeries2(num -2);
+  if (num < 1) return 0;
+  let result = Array(num).fill(0);
+  if (num >= 2) {
+    result[0] = 0;
+    result[1] = 1;
+    for (let i = 2; i < num; i++) {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+  }
+  return result[result.length-1];
+}
+console.log(fibonacciSeries2(2));
+console.log(fibonacciSeries2(4));
+console.log(fibonacciSeries2(8));
+console.log(fibonacciSeries2(9));
+console.log(fibonacciSeries2(1));
+console.log(fibonacciSeries2(3));
+/* Task 8
+Requirement:
+Write a function named findUniques() which takes two array of number
+arguments and returns the array which has only the unique values from both
+given arrays.
+NOTE: If both arrays are empty, then return an empty array.
+NOTE: If one of the array is empty, then return unique values from the other
+array.
+Examples:
+findUniques([], [])  -> []
+findUniques([], [1, 2, 3, 2])  -> [1, 2, 3]
+findUniques([1, 2, 3, 4], [3, 4, 5, 5])  -> [1, 2, 5]
+findUniques([8, 9], [9, 8, 9])  -> []
+findUniques([-1, -2], [1, 2])  -> [-1, -2, 1, 2]
+ */
+console.log('\nOutput for Task8');
+
+function findUniques(arr1, arr2) {
+  let arrLen = Math.max(arr1.length, arr2.length);
+  let result = [];
+  for (let i = 0; i < arrLen; i++) {
+    if (arr1[i] && !arr2.includes(arr1[i]) && !result.includes(arr1[i]))
+      result.push(arr1[i]);
+    if (!arr1.includes(arr2[i]) && !result.includes(arr2[i]))
+      result.push(arr2[i]);
+  }
+  return result;
+}
+console.log(findUniques([], []));
+console.log(findUniques([], [1, 2, 3, 2]));
+console.log(findUniques([1, 2, 3, 4], [3, 4, 5, 5]));
+console.log(findUniques([8, 9], [9, 8, 9]));
+console.log(findUniques([-1, -2], [1, 2]));
+/* Task9
+ Requirement:
+Write a function named isPowerOf3() which takes a number argument and returns true if given number is equal to 3 power of the X. Otherwise, return
+false.
+NOTE: Numbers that are power of 3 = 1, 3, 9, 27, 81, 243....
+NOTE: Ignore negative scenarios.
+Examples:
+isPowerOf3(1)  -> true
+isPowerOf3(2)  -> false
+isPowerOf3(3)  -> true
+isPowerOf3(27)  -> true
+isPowerOf3(100)  -> false
+isPowerOf3(81)  -> true
+isPowerOf3(9)  -> true*/
+
+console.log('\nOutput for Task9');
+
+function isPowerOf3(num) {
+  if (num === 1) return true;
+  let i = 3;
+  while (i <= num) {
+    if (i === num) return true;
+    i *= 3;
+  }
+  return false;
+}
+console.log(isPowerOf3(1));
+console.log(isPowerOf3(2));
+console.log(isPowerOf3(3));
+console.log(isPowerOf3(27));
+console.log(isPowerOf3(100));
+console.log(isPowerOf3(81));
+console.log(isPowerOf3(9));
